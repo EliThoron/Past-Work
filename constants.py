8e3d4c11cfe540a181878d0c255961c965f42dde
EliThoron <82829400+EliThoron@users.noreply.github.com>
2021-04-21 08:49:34 -0700

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,16 @@
+AUTH_TOKEN = ''
+
+# User IDs
+MIKU_ID =       # (Discord Bot ID)
+
+#voice channel IDs 
+VC_HOME =        # VC to join on startup
+VC_PRIVATE =     # private VC
+VC_MOVIE =       # Movie VC
+
+#text channel IDs
+TEXT_DEFAULT =   # Main channel
+TEXT_PRIVATE =   # private text channel
+TEXT_MOVIE =     # movie-night-TextChannel
+TEXT_LOGGING =   # Audit/log channel
+
